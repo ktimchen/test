3d8e0432abe1310974aa563e850b2df62c84c16c
ktimchen <ktimchen2@gmail.com>
2020-11-07 17:03:20 -0500

    new one

---
+++ app.py
@@ -0,0 +1,32 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open("model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    # return hello 
+    
+@app.route("/predict", method = ["POST"])
+
+def predict():
+    #takes the html input
+    int_feature = [int(x) for x in request_form.values()]
+    # make it an array
+    final_feature = [np.array(int_feature)]
+    
+    prediction = model.predict(final_feature)
+    
+    output = round(prediction[0], 2)
+    
+    #return the template to the html
+    return render_template("index.html", prediction_text = "well hello = {}".format(output))
+
+
+# run the app
+if __name__ == "__main__":
+    app.run(debug = True)
